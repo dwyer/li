d94032f72973b62a3299e7568a001a6c879d3dd3
Casey Dwyer <caseydwyer@gmail.com>
2012-02-08 06:11:55 -0800

    Clean up.

--- src/input.c
+++ src/input.c
@@ -20,18 +20,33 @@
 
 #define read_quote(f)   cons(symbol("quote"), cons(read(f), nil))
 
+int getch(FILE *f);
+int is_atom_number(const char *s);
+
+object *read_atom(FILE *f);
+object *read_sequence(FILE *f);
+object *read_special(FILE *f);
+object *read_string(FILE *f);
+
+/*
+ * Returns the beginning of the next object.
+ */
 int getch(FILE *f) {
     int c;
 
     while (isspace(c = getc(f)));
     if (iscomment(c)) {
-        while (!iseol(c = getc(f)));
+        while (!iseol(c = getc(f)))
+            ;
         return getch(f);
     }
     return c;
 }
 
-int token_is_number(const char *tok) {
+/*
+ * Returns non-zero if the given string can be converted to a number.
+ */
+int is_atom_number(const char *s) {
     int ret;
     int c;
     int isfloat;
@@ -40,9 +55,9 @@ int token_is_number(const char *tok) {
     ret = 0;
     isfloat = 0;
     hasdigit = 0;
-    if (*tok == '-')
-        tok++;
-    while ((c = *tok++) != '\0') {
+    if (*s == '-')
+        s++;
+    while ((c = *s++) != '\0') {
         if (isdigit(c))
             hasdigit = 1;
         else if (c == '.' && !isfloat)
@@ -52,6 +67,85 @@ int token_is_number(const char *tok) {
         ret = 1;
     }
     return ret && hasdigit;
+}
+
+object *read(FILE *f) {
+    int c;
+
+    if (iseof(c = getch(f)))
+        return eof;
+    else if (isquote(c))
+        return read_quote(f);
+    else if (issharp(c))
+        return read_special(f);
+    else if (isstring(c))
+        return read_string(f);
+    else if (isopener(c))
+        return read_sequence(f);
+    else if (iscloser(c))
+        error("read", "unmatched right brace", nil);
+    ungetc(c, f);
+    return read_atom(f);
+}
+
+object *read_atom(FILE *f) {
+    object *ret;
+    char *buf;
+    int buf_sz;
+    int i, c;
+
+    i = 0;
+    buf_sz = BUF_SZ;
+    buf = calloc(buf_sz, sizeof(*buf));
+    do {
+        buf[i++] = c = getc(f);
+        if (i == buf_sz) {
+            buf_sz *= 2;
+            buf = realloc(buf, buf_sz * sizeof(*buf));
+        }
+    } while (!isdelimiter(c));
+    buf[i-1] = '\0';
+    ungetc(c, f);
+    if (is_atom_number(buf))
+        ret = number(atof(buf));
+    else
+        ret = symbol(buf);
+    free(buf);
+    return ret;
+}
+
+object *read_sequence(FILE *f) {
+    object *obj;
+    int c;
+
+    if (iscloser(c = getch(f)))
+        return nil;
+    else if (c == '.') {
+        obj = read(f);
+        if (iscloser(c = getch(f)))
+            return obj;
+        else
+            error("read", "ill-formed dotted pair", nil);
+    }
+    ungetc(c, f);
+    obj = read(f);
+    return cons(obj, read_sequence(f));
+}
+
+object *read_special(FILE *f) {
+    int c;
+
+    c = getc(f);
+    if (c == 't') {
+        return boolean(true);
+    } else if (c == 'f') {
+        return boolean(false);
+    } else if (isopener(c)) {
+        ungetc(c, f);
+        return vector(read(f));
+    } else {
+        return nil; /* TODO: something better */
+    }
 }
 
 object *read_string(FILE *f) {
@@ -76,82 +170,4 @@ object *read_string(FILE *f) {
     ret = string(buf);
     free(buf);
     return ret;
-}
-
-object *read_token(FILE *f) {
-    object *ret;
-    char *buf;
-    int buf_sz;
-    int i, c;
-
-    i = 0;
-    buf_sz = BUF_SZ;
-    buf = calloc(buf_sz, sizeof(*buf));
-    do {
-        buf[i++] = c = getc(f);
-        if (i == buf_sz) {
-            buf_sz *= 2;
-            buf = realloc(buf, buf_sz * sizeof(*buf));
-        }
-    } while (!isdelimiter(c));
-    buf[i-1] = '\0';
-    ungetc(c, f);
-    if (token_is_number(buf))
-        ret = number(atof(buf));
-    else
-        ret = symbol(buf);
-    free(buf);
-    return ret;
-}
-
-object *read_sharp(FILE *f) {
-    int c;
-
-    c = getc(f);
-    if (c == 't')
-        return boolean(true);
-    else if (c == 'f')
-        return boolean(false);
-    else if (isopener(c)) {
-        ungetc(c, f);
-        return vector(read(f));
-    } else
-        return nil; /* TODO: something better */
-}
-
-object *read_sequence(FILE *f) {
-    object *obj;
-    int c;
-
-    if (iscloser(c = getch(f)))
-        return nil;
-    else if (c == '.') {
-        obj = read(f);
-        if (iscloser(c = getch(f)))
-            return obj;
-        else
-            error("read", "ill-formed dotted pair", nil);
-    }
-    ungetc(c, f);
-    obj = read(f);
-    return cons(obj, read_sequence(f));
-}
-
-object *read(FILE *f) {
-    int c;
-
-    if (iseof(c = getch(f)))
-        return eof;
-    else if (isquote(c))
-        return read_quote(f);
-    else if (issharp(c))
-        return read_sharp(f);
-    else if (isstring(c))
-        return read_string(f);
-    else if (isopener(c))
-        return read_sequence(f);
-    else if (iscloser(c))
-        error("read", "unmatched right brace", nil);
-    ungetc(c, f);
-    return read_token(f);
 }
